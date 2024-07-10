8a3131e4ec33ef7989b940155378141159d5b92b
iptnkt <iptnikitagc@gmail.com>
2024-07-10 14:31:04 +0300

    UP-1-VRCMigrate
    Rebuild prodgect from Spatial to VRChat

---
+++ Assets/Scripts/CubeAnimator.cs
@@ -0,0 +1,20 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class CubeAnimator : UdonSharpBehaviour
+{
+    public Animator cubeAnimator;
+    public string parEnterName;
+    
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        if(player==Networking.LocalPlayer) cubeAnimator.SetBool(parEnterName, true);
+    }
+    public override void OnPlayerTriggerExit(VRCPlayerApi player)
+    {
+        if (player == Networking.LocalPlayer) cubeAnimator.SetBool(parEnterName, false);
+    }
+}
